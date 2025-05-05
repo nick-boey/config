5eddc9de69a5a1596a2d4d341cee006a56137110
Nick Boey <nboey@northrop.com.au>
2025-05-06 08:56:03 +1000

    Add Linux dotfiles

---
+++ .windows/wezterm/.wezterm.lua
@@ -0,0 +1,72 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.default_prog = { 'pwsh.exe' }
+
+config.color_scheme = 'JetBrains Darcula'
+config.colors = {
+	background = "#1f1f1f",
+}
+
+config.warn_about_missing_glyphs = false
+config.hide_tab_bar_if_only_one_tab = true
+
+config.keys = {
+	{
+		key = "I",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitPane({
+			top_level = true,
+			direction = "Right",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "O",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitPane({
+			top_level = true,
+			direction = "Down",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "s",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.PaneSelect {
+			mode = "SwapWithActive"
+		}
+	},
+	{
+		key = "q",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action({ CloseCurrentPane = { confirm = false } }),
+	},
+	{
+		key = "h",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "j",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "k",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "l",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = 'F11',
+		action = wezterm.action.ToggleFullScreen,
+	}
+}
+
+return config
