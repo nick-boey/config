4f31cd1e8c3b5acac3a5da9222b4c29644137bb6
nick-boey <nboey@northrop.com.au>
2025-10-06 10:43:41 +1100

    Attempt to add Suset language to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,16 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.filetype.add({
+  extension = {
+    sunset = "sunset",
+  },
+})
+
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.sunset = {
+  install_info = {
+    url = "https://github.com/sunset-lang/tree-sitter-sunset",
+    branch = "dev",
+  },
+}
